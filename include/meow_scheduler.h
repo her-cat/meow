fce26e094d094ab6e5985b5df46ea1c8c8cdc1e4
her-cat <i@her-cat.com>
2021-03-11 18:43:16 +0800

    Remove libuv function declare

--- include/meow_scheduler.h
+++ include/meow_scheduler.h
@@ -4,15 +4,6 @@
 #include <uv.h>
 #include "meow_coroutine.h"
 
-typedef enum {
-    UV_CLOCK_PRECISE = 0,
-    UV_CLOCK_FAST = 1
-} uv_clocktype_t;
-
-void uv__run_timers(uv_loop_t *loop);
-uint64_t uv__hrtime(uv_clocktype_t type);
-int uv__next_timeout(const uv_loop_t *loop);
-
 meow_coroutine_t *meow_coroutine_scheduler_module_init();
 void meow_coroutine_scheduler_run();
 
